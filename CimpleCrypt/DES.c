11c566af12bd99f88ec56f6f369ae3d3df760588
DelSigno <del.signo.srl@gmail.com>
2014-10-05 18:42:23 -0400

    Transfering to new laptop

--- CimpleCrypt/DES.c
+++ CimpleCrypt/DES.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
 
-void encrypeWithDES(FILE *plainText, FILE *cipherText, char[]){
+void encrypeWithDES(FILE *plainText, FILE *cipherText){
+	
+	//The super dooper magical key!
+	char key[7];
 
+	//The various blocks.
+	char currentBlock[8];
+	char currentLeftBlock[4];
+	char currentRightBlock[4];
+	char nextLeftBlock[4];
+	char nextRightBlock[4];
+
+	//#iterationthings
+	int encrypting = 1;
+
+	//Checks to make sure that our plaintext file is valid
+	if (plainText == NULL) {
+		perror("Failed to open file \"myfile\"");
+		getchar();
+		return;
+	}
+	while (encrypting){
+		//Begins by getting the first encryption block
+		for (int i = 0; i < 7; i++){
+			char currentChar = fgetc(plainText);
+			
+			//Checks to see if we have reached the end of file.
+			if (currentChar == EOF) {
+				fputs("\nAn error occurred while reading the file.\n", stderr);
+				getchar();
+				return;
+			}
+			else{
+				currentBlock[i] = currentChar;
+			}
+			printf("%c", currentChar);
+		}
+		//Finished populating block
+
+		//splits our current block into two the left and right block
+		for (int i = 0; i < 3; i++){
+			currentLeftBlock[i] = currentBlock[i];
+			currentRightBlock[i] = currentBlock[i + 3];
+		}
+
+		//Begins the actuall encrypting process, 
+		for (int i = 0; i < 15; i++){
+
+		}
+	}
 }
 
-void decryptWithDES()
+void decryptWithDES(FILE *plainText, FILE *cipherText){
+	return;
+}
+
+char *theFuntion(char *block, char *subKey){
+	
+	//expander
+}
+
--- CimpleCrypt/main.c
+++ CimpleCrypt/main.c
@@ -3,6 +3,7 @@
 int main()
 {
 	FILE *fileToEncrypt = fopen("C:\\test\\testtext", "rb");
+	FILE *fileToEncrypt02 = fopen("C:\\test\\testtext02", "w+");
 	int fileFinished = 0;
 	//char []
 	char testMatrix[] = { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4};
@@ -13,14 +14,16 @@ int main()
 		getchar();
 		return 0;
 	}
-
+	
 	while (!fileFinished){
 		char currentChar = fgetc(fileToEncrypt);
 		if (currentChar == EOF) {
 			fputs("\nAn error occurred while reading the file.\n", stderr);
+			putc('A', fileToEncrypt);
 			getchar();
 			return 0;
 		}
+		fputc((int)currentChar, fileToEncrypt02);
 		printf("%c", currentChar);
 	}
 
