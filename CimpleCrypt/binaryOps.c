91c711091864b9b02a16ccc3cae2e5bf15002405
delsigno <keithpapa116@gmail.com>
2014-10-05 22:48:05 -0400

    hopeing for the best

--- CimpleCrypt/binaryOps.c
+++ CimpleCrypt/binaryOps.c
@@ -1,7 +1,14 @@
 // Keith Papa
 // 10/4/2014
 
-
+#define BIT_ONE 0x01
+#define BIT_TWO 0x02
+#define BIT_THREE 0x04
+#define BIT_FOUR 0x08
+#define BIT_FIVE 0x10
+#define BIT_SIX 0x20
+#define BIT_SEVEN 0x40
+#define BIT_EIGHT 0x80
 
 
 // gets the bit from least to most significant.
@@ -12,5 +19,23 @@ int getBitFromCharray(char **charray, int bit){
 
 	return  ((*charray[targetChar] >> bitShift) & 0x01);
 
+
+}
+
+void setBitToCharray(char **charray, int bit, int newBit){
+	
+	int bitShift = bit % 8;
+	int targetChar = sizeof(*charray) - 1 - (bit / 8);
+
+	switch (bitShift){
+		case 0:
+			*charray[targetChar] = ((*charray[targetChar] >> bitShift) & 0x01);
+			break;
+		case 1:
+			break;
+		case 3:
+
+	}
+	
 
 }
